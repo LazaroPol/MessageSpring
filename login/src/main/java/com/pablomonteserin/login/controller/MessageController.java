050d605e54b226882a23cb17d529d3ba8a73f998
LazaroPol <pol.lazaro.rosas@gmail.com>
2024-11-08 19:46:46 +0100

    fwdwdwç

--- login/src/main/java/com/pablomonteserin/login/controller/MessageController.java
+++ login/src/main/java/com/pablomonteserin/login/controller/MessageController.java
@@ -1,4 +1,28 @@
 package com.pablomonteserin.login.controller;
 
+import com.pablomonteserin.login.persistence.entities.Message;
+import com.pablomonteserin.login.persistence.repository.MessageRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin // Para hacer peticiones desde otro servidor
+@RestController // Para hacer peticiones REST
+@RequestMapping("/messages")
 public class MessageController {
+
+    MessageRepository messageRepository;
+
+    @GetMapping
+    public List<Message> getMessageList(){
+        List<Message> messageList = messageRepository.findAll();
+        return messageList;
+
+    }
+
+    @PostMapping("/")
+    public void createMessage(@RequestBody Message message){
+    messageRepository.save(message);
+
+    }
 }
